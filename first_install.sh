3a52d0398efb570a95b4cb7f09ac190de92179c3
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2018-06-25 15:37:31 +0900

    画像編集ソフトのインストールを追加

--- first_install.sh
+++ first_install.sh
@@ -2,8 +2,12 @@
 
 sudo apt-get update
 
-
+#ドライバの設定
 sudo apt-get install nvidia-375
 
 #「システムプログラムの問題が見つかりました」が表示されないように設定する
 sudo rm -rf /var/crash/*
+
+#画像編集ソフトのインストール
+sudo apt-get install gimp -y
+sudo apt-get install pinta -y
